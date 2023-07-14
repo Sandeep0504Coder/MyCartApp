d4c59132096a5ba77e1cf80e72ef52268f1e64e4
Sandeep <sandeepmandal0504@gmail.com>
2023-07-15 03:13:59 +0530

    change the brand name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends React.Component {
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            Apna Cart
+            My Cart
           </a>
         </div>
       </nav>
